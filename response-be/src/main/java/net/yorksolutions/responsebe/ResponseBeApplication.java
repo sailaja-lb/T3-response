7a3344c7e955cd184a4bb32b9e1e7c7dc0e2eecc
GxBonus <bthao018@gmail.com>
2022-06-17 16:40:46 -0500

    response backend setup

---
+++ response-be/src/main/java/net/yorksolutions/responsebe/ResponseBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.responsebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseBeApplication.class, args);
+    }
+
+}
